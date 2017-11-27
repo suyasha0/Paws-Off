18148cf6d412377f59659f32a57759572a3a35ab
Suyasha Shrestha <ss8403@nyu.edu>
2017-11-27 13:08:37 -0500

    replace setup code using Prof Kapp's MoveInVR setup

--- sketch.js
+++ sketch.js
@@ -1,28 +1,70 @@
 // variable to hold a reference to our A-Frame world
 var world;
 
-var controller = new Leap.Controller();
-controller.connect();
+// our Leap motion hand sensor controller object (instantiated inside of 'setup');
+var leapController;
 
-controller.on('connect', onConnect);
-function onConnect()
-{
-    console.log("Hello there");
-}
+// our hand displays (simple shapes that will show up in front of the user)
+var hand1, hand2;
 
 function setup() {
-	// no canvas needed
-	noCanvas();
+  // no canvas needed
+  noCanvas();
 
-	world = new World('VRScene');
+  // construct the A-Frame world
+  // this function requires a reference to the ID of the 'a-scene' tag in our HTML document
+  world = new World('VRScene');
 
-	// create a plane to serve as our "ground"
-	var g = new Plane({x:0, y:0, z:0, width:100, height:100, red:0, green:102, blue:153, rotationX:-90, metalness:0.25});
+  // set up our leap controller
+  leapController = new Leap.Controller({
+    enableGestures: true
+  });
 
-	// add the plane to our world
-	world.add(g);
-}
+  // every time the Leap provides us with hand data we will ask it to run this function
+  leapController.loop(handleHandData);
 
-function draw() {
+  // create a plane to serve as our "ground"
+  var g = new Plane({
+    x: 0,
+    y: 0,
+    z: 0,
+    width: 100,
+    height: 100,
+    asset: 'stone',
+    repeatX: 100,
+    repeatY: 100,
+    rotationX: -90,
+    metalness: 0.25
+  });
 
+  // add the plane to our world
+  world.add(g);
+
+
+  // now add our hands to the world
+  hand1 = new Box({
+    x: -0.5,
+    y: 0,
+    z: -1,
+    width: 0.1,
+    height: 0.1,
+    depth: 0.1,
+    red: 255,
+    green: 0,
+    blue: 0
+  });
+  hand2 = new Box({
+    x: 0.5,
+    y: 0,
+    z: -1,
+    width: 0.1,
+    height: 0.1,
+    depth: 0.1,
+    red: 0,
+    green: 255,
+    blue: 0
+  });
+  // add the hands to our camera - this will force it to always show up on the user's display
+  world.camera.holder.appendChild(hand1.tag);
+  world.camera.holder.appendChild(hand2.tag);
 }
